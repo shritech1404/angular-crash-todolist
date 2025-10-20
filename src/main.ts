e9cbe1e904e1596b8c82db4bf33fbcd0a4d518c9
Shrikant Shejwal <shrikantshejwal12345@gmail.com>
2025-10-20 16:00:47 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { enableProdMode } from '@angular/core';
-hello world
+import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
