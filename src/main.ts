746f1ef427899f1add4782679a830c046dc6b24a
Shrikant Shejwal <shrikantshejwal12345@gmail.com>
2025-10-30 17:38:23 +0530

    Add print statement for debugging

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+print("angularCrash")
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
